cd4f7be61fae98f02e8ecf77ddb0552c580c56d3
salhugo <hugo.salinasmarty@ynov.com>
2025-11-26 16:06:36 +0100

    ajout de la page du reglement

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Accueil from './pages/Accueil.jsx';
+import Reglement from './pages/Reglement.jsx'
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Accueil />} />
+      <Route path="/reglement" element={<Reglement />} />
     </Routes>
   );
 }
---
+++ frontend/src/pages/Reglement.jsx
@@ -0,0 +1,10 @@
+export default function Reglement() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>Règlement du serveur</h1>
+      <p>
+        Ici vous mettez votre règlement (no trolling, pas de haine, etc.).
+      </p>
+    </div>
+  );
+}
